fb9fe12edebe51d0adecd2778e264d5b21b239cd
Darwin Subramaniam <darwinsubramaniam@googlemail.com>
2023-05-14 12:43:12 +0800

    update fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,31 +3,25 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/cash.tsx";
-import * as $4 from "./routes/defi.tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/staking.tsx";
-import * as $$0 from "./islands/Counter.tsx";
-import * as $$1 from "./islands/Footer.tsx";
-import * as $$2 from "./islands/Header.tsx";
+import * as $0 from "./routes/_app.tsx";
+import * as $1 from "./routes/cash.tsx";
+import * as $2 from "./routes/defi.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/staking.tsx";
+import * as $$0 from "./islands/Footer.tsx";
+import * as $$1 from "./islands/Header.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/cash.tsx": $3,
-    "./routes/defi.tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/staking.tsx": $6,
+    "./routes/_app.tsx": $0,
+    "./routes/cash.tsx": $1,
+    "./routes/defi.tsx": $2,
+    "./routes/index.tsx": $3,
+    "./routes/staking.tsx": $4,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
-    "./islands/Footer.tsx": $$1,
-    "./islands/Header.tsx": $$2,
+    "./islands/Footer.tsx": $$0,
+    "./islands/Header.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
